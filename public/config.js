421fa3fe71809e01f19ffbfdc4fb8b81bbac586c
linkkk6 <89188112+2319564679@users.noreply.github.com>
2022-08-12 22:21:36 +0800

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'ZCOOL旗下服务状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1774167-39868d51ae0e20fb6b157432',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://jk.zcool.org.cn/'
+    },
+    {
+      text: 'ZCOOL官网',
+      url: 'https://zcool.org.cn/'
+    },
+  ],
+};
